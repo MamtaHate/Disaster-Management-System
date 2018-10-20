b30e15e9a6d545ff07dfcb160c58c50c6308e99d
prathamesh-tambe <tambe.p@husky.neu.edu>
2018-10-19 20:19:44 -0400

    space issue

--- Lab5/src/UserInterface/AdminCreateScreen.java
+++ Lab5/src/UserInterface/AdminCreateScreen.java
@@ -37,7 +37,8 @@ public class AdminCreateScreen extends javax.swing.JPanel {
         this.panelRight = panelRight;
         this.admin = admin;
     }
-
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always

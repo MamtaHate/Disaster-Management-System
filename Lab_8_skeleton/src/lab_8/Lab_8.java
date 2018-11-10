e66844f23ec6eb7e2ab9f3d6694aec5450f6f06e
prathamesh-tambe <tambe.p@husky.neu.edu>
2018-11-09 20:41:18 -0500

    second average logic added

--- Lab_8_skeleton/src/lab_8/Lab_8.java
+++ Lab_8_skeleton/src/lab_8/Lab_8.java
@@ -89,6 +89,7 @@ public class Lab_8 {
         helper.userWithMostLikes();
         helper.getFiveMostLikedComment();
         helper.averageNumberOfLikesPerComment();
+        helper.avgLikesPerComment();
         helper.postWithMostLikedComments();
         helper.postWithMostComments();
         helper.top5InactiveUsersBasedOnComments();

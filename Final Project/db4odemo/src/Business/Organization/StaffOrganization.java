4443fa4e1bad636f09abc10d3d7f3a4959f049cc
Sweta Chowdhury <chowdhury.sw@husky.neu.edu>
2018-12-01 03:46:05 -0500

    Changes related to Staff organization and Relief Enterprise

---
+++ Final Project/db4odemo/src/Business/Organization/StaffOrganization.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import Business.Role.Role;
+import Business.Role.StaffRole;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sweta Chowdhury
+ */
+public class StaffOrganization extends Organization {
+   
+    public StaffOrganization() {
+        super(OrganizationType.Staff.getValue());
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new StaffRole());
+        return roles;
+    }
+    
+}

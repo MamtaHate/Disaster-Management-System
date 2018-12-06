33c5aa6a943873f776eed35d018439740687dc1a
Sweta Chowdhury <chowdhury.sw@husky.neu.edu>
2018-12-06 00:37:25 -0500

    add swing jar file

--- Final Project/db4odemo/src/userinterface/ReliefOrgAdmin/ApproveDonationsJPanel.java
+++ Final Project/db4odemo/src/userinterface/ReliefOrgAdmin/ApproveDonationsJPanel.java
@@ -11,6 +11,8 @@ import Business.Incident.Incident;
 import Business.UserAccount.UserAccount;
 import Business.WorkQueue.DonationWorkRequest;
 import Business.WorkQueue.WorkRequest;
+import java.awt.CardLayout;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
 
@@ -19,20 +21,22 @@ import javax.swing.table.DefaultTableModel;
  * @author Sweta Chowdhury
  */
 public class ApproveDonationsJPanel extends javax.swing.JPanel {
-    private JPanel container;
+    private JPanel userProcessContainer;
     private Enterprise enterprise;
     private UserAccount account;
     private EcoSystem system;
     /**
      * Creates new form ApproveDonationsJPanel
      */
-    public ApproveDonationsJPanel(JPanel container, Enterprise enterprise, UserAccount account, EcoSystem system) {
+    public ApproveDonationsJPanel(JPanel userProcessContainer, Enterprise enterprise, UserAccount account, EcoSystem system) {
         initComponents();
         this.enterprise = enterprise;
-        this.container = container;
+        this.userProcessContainer = userProcessContainer;
         this.account = account;
         this.system = system;
         populateTable();
+        donationRequests.getColumnModel().getColumn(0).setMinWidth(0);
+        donationRequests.getColumnModel().getColumn(0).setMaxWidth(0);
     }
 
     
@@ -48,6 +52,7 @@ public class ApproveDonationsJPanel extends javax.swing.JPanel {
                 row[2] = request.getSender();
                 row[3] = request.getReceiver();
                 row[4] = request.getResolveDate();
+                row[5] = request.getStatus();
                 model.addRow(row);
         }
       
@@ -66,31 +71,70 @@ public class ApproveDonationsJPanel extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         donationRequests = new javax.swing.JTable();
+        backJButton = new javax.swing.JButton();
+        approveButton = new javax.swing.JButton();
 
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
         jLabel1.setText("APPROVE DONATIONS");
-        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 40, 280, 41));
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 40, 280, 41));
 
         donationRequests.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Donation", "Donation Type", "Sender ", "Approver", "Approved Date", "Status"
             }
         ));
         jScrollPane1.setViewportView(donationRequests);
 
-        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(232, 130, 660, 140));
+        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(212, 130, 780, 140));
+
+        backJButton.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        backJButton.setText("<<BACK");
+        backJButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backJButtonActionPerformed(evt);
+            }
+        });
+        add(backJButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 400, 170, 50));
+
+        approveButton.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+        approveButton.setText("Approve");
+        approveButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                approveButtonActionPerformed(evt);
+            }
+        });
+        add(approveButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 290, 160, 40));
     }// </editor-fold>//GEN-END:initComponents
+
+    private void backJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backJButtonActionPerformed
+        userProcessContainer.remove(this);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.previous(userProcessContainer);
+    }//GEN-LAST:event_backJButtonActionPerformed
+
+    private void approveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_approveButtonActionPerformed
+        // TODO add your handling code here:
+        if (donationRequests.getSelectedRow() >= 0) {
+           WorkRequest request = (DonationWorkRequest)donationRequests.getValueAt(donationRequests.getSelectedRow() , 0);
+           request.setReceiver(account);
+           request.setStatus("Approved");
+           populateTable();
+        }
+        else {
+            JOptionPane.showMessageDialog(this, "Please select an incident");
+        }
+        
+    }//GEN-LAST:event_approveButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton approveButton;
+    private javax.swing.JButton backJButton;
     private javax.swing.JTable donationRequests;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
